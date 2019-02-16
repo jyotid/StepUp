9f9d048a8f7d756acf6e4c7d3a6be011b77d84a7
Jyoti Dubey <jyoti0801@gmail.com>
2019-02-16 17:02:36 +0530

    Add Category import

--- app/src/main/java/com/womandroid/stepup/LoginActivity.java
+++ app/src/main/java/com/womandroid/stepup/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.womandroid.stepup.categories.CategoryActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
