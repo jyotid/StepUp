06a1063f0ac6f6a51c6a93939dd1d445e7042606
ekta.nijhawan <ektanijhawan1@gmail.com>
2019-02-16 17:00:39 +0530

    Update : Profile Desc

--- app/src/main/java/com/womandroid/stepup/ProfileDescriptionActivity.kt
+++ app/src/main/java/com/womandroid/stepup/ProfileDescriptionActivity.kt
@@ -2,10 +2,14 @@ package com.womandroid.stepup
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.ViewCompat
 import android.support.v7.widget.LinearLayoutManager
 import android.widget.LinearLayout
 import kotlinx.android.synthetic.main.activity_profile_description.*
 import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.RecyclerView
+
+
 
 
 
@@ -22,11 +26,19 @@ class ProfileDescriptionActivity : AppCompatActivity() {
         rvCourses.adapter = CoursesAdapter(this)
         rvCourses.setItemAnimator(DefaultItemAnimator())
 
+//        val layoutManager = object : LinearLayoutManager(this) {
+//            override fun canScrollVertically(): Boolean {
+//                return false
+//            }
+//        }
+//        rvQuestions.setLayoutManager(layoutManager)
 
-        rvQuestions.layoutManager = LinearLayoutManager(this)
-        rvQuestions.isNestedScrollingEnabled = false
+        rvQuestions.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+      //  rvQuestions.isNestedScrollingEnabled = false
         rvQuestions.adapter = QuestionAskedAdpter(this)
         rvQuestions.setItemAnimator(DefaultItemAnimator())
+        ViewCompat.setNestedScrollingEnabled(rvQuestions, false);
+
 
     }
 }
