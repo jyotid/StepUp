3dd666706ad0d13d13db6df43bed3a093deeddb6
ekta.nijhawan <ektanijhawan1@gmail.com>
2019-02-16 18:18:35 +0530

    Add: Ask Question Dialog UI

--- app/src/main/java/com/womandroid/stepup/profile/ProfileDescriptionActivity.kt
+++ app/src/main/java/com/womandroid/stepup/profile/ProfileDescriptionActivity.kt
@@ -1,13 +1,20 @@
 package com.womandroid.stepup.profile
 
-import android.support.v7.app.AppCompatActivity
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.support.v4.view.ViewCompat
-import android.support.v7.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.activity_profile_description.*
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.LinearLayoutManager
+import android.view.Window
+import android.widget.Button
+import android.widget.EditText
 import com.womandroid.stepup.QuestionAskedAdpter
 import com.womandroid.stepup.R
+import kotlinx.android.synthetic.main.activity_profile_description.*
+import android.view.ViewGroup
 
 
 class ProfileDescriptionActivity : AppCompatActivity() {
@@ -21,21 +28,41 @@ class ProfileDescriptionActivity : AppCompatActivity() {
     private fun initViews() {
         rvCourses.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         rvCourses.adapter = CoursesAdapter(this)
-        rvCourses.setItemAnimator(DefaultItemAnimator())
+        rvCourses.itemAnimator = DefaultItemAnimator()
 
-//        val layoutManager = object : LinearLayoutManager(this) {
-//            override fun canScrollVertically(): Boolean {
-//                return false
-//            }
-//        }
-//        rvQuestions.setLayoutManager(layoutManager)
 
         rvQuestions.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-      //  rvQuestions.isNestedScrollingEnabled = false
         rvQuestions.adapter = QuestionAskedAdpter(this)
-        rvQuestions.setItemAnimator(DefaultItemAnimator())
-        ViewCompat.setNestedScrollingEnabled(rvQuestions, false);
+        rvQuestions.itemAnimator = DefaultItemAnimator()
+        ViewCompat.setNestedScrollingEnabled(rvQuestions, false)
+
+        tvAskQuestion.setOnClickListener {
+            showAskQuestionDialog()
+        }
 
 
+    }
+
+    private fun showAskQuestionDialog() {
+
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.dialog_ask_question)
+        dialog.window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT));
+
+        val btnSubmit = dialog.findViewById(R.id.btnSubmitQuestion) as Button
+        val etAskQuestion = dialog.findViewById(R.id.etAskQuestion) as EditText
+        btnSubmit.setOnClickListener {
+            dialog.cancel()
+        }
+
+        dialog.setCancelable(true)
+        dialog.show()
+
+        val window = dialog.getWindow()
+        window.setLayout(
+                ViewGroup.LayoutParams.FILL_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT)
     }
 }
