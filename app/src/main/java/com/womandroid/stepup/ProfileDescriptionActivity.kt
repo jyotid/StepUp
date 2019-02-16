aee049401655c8e8729af531bbf5a2449b590316
ekta.nijhawan <ektanijhawan1@gmail.com>
2019-02-16 15:21:03 +0530

    Update: Profile Desc

---
+++ app/src/main/java/com/womandroid/stepup/ProfileDescriptionActivity.kt
@@ -0,0 +1,12 @@
+package com.womandroid.stepup
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileDescriptionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_description)
+    }
+}
