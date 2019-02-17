46f9e9fa3c6d9aa9e9f2580b57a7fc79dbb3f155
Jyoti Dubey <jyoti0801@gmail.com>
2019-02-17 14:12:42 +0530

    Update Question and courses adapter

--- app/src/main/java/com/womandroid/stepup/QuestionAskedAdpter.kt
+++ app/src/main/java/com/womandroid/stepup/QuestionAskedAdpter.kt
@@ -5,21 +5,26 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.womandroid.stepup.data.QuestionsDataModel
+import kotlinx.android.synthetic.main.item_question.view.*
 
-class QuestionAskedAdpter(val context: Context) : RecyclerView.Adapter<QuestionViewHolder>() {
+class QuestionAskedAdpter(val context: Context, val data: List<QuestionsDataModel>) : RecyclerView.Adapter<QuestionViewHolder>() {
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): QuestionViewHolder {
 
         return QuestionViewHolder(LayoutInflater.from(context).inflate(R.layout.item_question, null, false))
     }
 
-    override fun onBindViewHolder(viewHolde: QuestionViewHolder, p1: Int) {
+    override fun onBindViewHolder(viewHolder: QuestionViewHolder, p1: Int) {
+        viewHolder.question?.text = data[p1].question
+        viewHolder.answer?.text = data[p1].answer
     }
 
-    // Gets the number of animals in the list
     override fun getItemCount(): Int {
-        return 5
+        return data.size
     }
 }
 
 class QuestionViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    val question = itemView?.question
+    val answer = itemView?.answer
 }
